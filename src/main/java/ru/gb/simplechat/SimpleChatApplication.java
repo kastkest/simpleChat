9cda50791d72142583d352afd566be687cf36c49
kastkest <endimion-xi@yandex.ru>
2022-01-19 22:05:37 +0300

    Create Chat Project v1.0

--- src/main/java/ru/gb/simplechat/SimpleChatApplication.java
+++ src/main/java/ru/gb/simplechat/SimpleChatApplication.java
@@ -1,4 +1,4 @@
-package java.ru.gb.simplechat;
+package ru.gb.simplechat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,17 +7,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SimpleChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(SimpleChatApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        stage.setTitle("SimpleChat v1.0");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
